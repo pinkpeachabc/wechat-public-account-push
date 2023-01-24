b04834f0a8adcdc212b0ee7d8231510523260573
Mason Lee <1183134682@qq.com>
2023-01-24 15:56:48 +0800

    🎉 init: wechat push

---
+++ config/index.cjs
@@ -0,0 +1,72 @@
+/* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】
+ * 自行添加属性，以支持更多个性化功能
+ */
+const USER_CONFIG = {
+  USE_PASSAGE: 'wechat-enterprise',
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: '',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: '',
+
+  PROVINCE: '上海',
+  CITY: '上海',
+
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: 'Mason',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      corpid: 'ww37e09043f1046156',
+
+      corpsecret: 'QmjN_qzX1eFUPivYARG0T6eHEfzLgTAibhZQwrww0OA',
+
+      agentid: '1000002',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0001',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '04-16',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日',
+          name: 'Doria',
+          year: '2000',
+          date: '04-16',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日',
+          name: 'Mason',
+          year: '1999',
+          date: '11-15',
+        }
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-05-12' },
+        // 结婚纪念日
+        // { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+  ],
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: '0002',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oX0kI6FcTF8e_ryu8CagPTCh2DT4',
+    },
+  ],
+}
+
+module.exports = USER_CONFIG
